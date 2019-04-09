e01f6efa99a180b7ec019145169b671945428eec
Leandro A. Siqueira <leandroaps@gmail.com>
2019-04-09 18:42:08 -0300

    Linting and Polling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
 import ReduxToastr from 'react-redux-toastr';
+import React from 'react';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import PricesComponent from './components/PricesComponent';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="container-fluid pr-0 pl-0">
-        <PricesComponent text="My Text" />
-        <ReduxToastr
-          transitionIn="fadeIn"
-          transitionOut="fadeOut"
-          preventDuplicates
-          timeOut={99999}
-        />
-      </div>
-    );
-  }
+class App extends React.PureComponent {
+    render() {
+        return (
+            <div className="container-fluid pr-0 pl-0">
+                <PricesComponent text="My Text" />
+                <ReduxToastr
+                    transitionIn="fadeIn"
+                    transitionOut="fadeOut"
+                    position="bottom-right"
+                    preventDuplicates
+                    timeOut={99999}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,23 +4,23 @@ import { toastr } from 'react-redux-toastr';
 export const LOAD_DATA_SUCCESS = 'LOAD_DATA_SUCCESS';
 
 export const loadPrices = () => dispatch => {
-  return fetch(
-    'https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,tsla,msft,googl,amzn&types=quote'
-  )
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
+    return fetch(
+        'https://api.iextrading.com/1.0/stock/market/batch?symbols=aapl,fb,tsla,msft,googl,amzn&types=quote'
+    )
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
 
-      throw new Error(response.statusText);
-    })
-    .then(
-      data => {
-        toastr.removeByType('error');
-        dispatch({ type: LOAD_DATA_SUCCESS, data });
-      },
-      error => {
-        toastr.error(`Error loading data: ${error.message}`);
-      }
-    );
+            throw new Error(response.statusText);
+        })
+        .then(
+            data => {
+                toastr.removeByType('error');
+                dispatch({ type: LOAD_DATA_SUCCESS, data });
+            },
+            error => {
+                toastr.error(`Error loading data: ${error.message}`);
+            }
+        );
 };
